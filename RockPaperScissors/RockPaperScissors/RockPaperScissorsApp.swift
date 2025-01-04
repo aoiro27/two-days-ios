5405ca97990082bb40021a276d500b1e7b5701d1
yuki.u <aoiro.savant@gmail.com>
2025-01-04 11:45:21 +0900

    アプリの初期設定ファイルと基本的な構造を追加

---
+++ RockPaperScissors/RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Yuki Usui on 2025/01/04.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
