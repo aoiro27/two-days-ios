4165688d30cd5608248f058f4a83ca87cc8fc43a
yuki.u <aoiro.savant@gmail.com>
2025-01-04 11:45:32 +0900

    じゃんけん機能を追加し、ランダムな手を表示するボタンを実装

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let te = [(image:"gu",text:"グー"), (image:"choki",text:"チョキ"), (image:"pa",text:"パー")]
+    
+    @State
+    var rand : Int?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+            if let rand {
+                
+                Image(te[rand].image)
+                    .resizable()
+                    .scaledToFit()
+                Text(te[rand].text)
+            } else {
+                Text("これからじゃんけんをします")
+                    .padding(.bottom)
+            }
+            Button(action: {
+                self.rand = Int.random(in: 0..<te.count)
+            }, label: {
+                Text("じゃんけんをする")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 100)
+                    .font(.title)
+                    .background(.pink)
+                    .foregroundColor(.white)
+                    
+            }
+            )
         }
-        .padding()
     }
 }
 
